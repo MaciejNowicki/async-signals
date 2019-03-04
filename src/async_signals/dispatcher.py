049104bf04bfe4858abcfc35d01d8b1f50f45a02
MaciejNowicki <maciejjnowicki@gmail.com>
2019-03-04 17:41:50 +0100

    Fix indents + add logging

--- src/async_signals/dispatcher.py
+++ src/async_signals/dispatcher.py
@@ -19,20 +19,28 @@ class AsyncSignal(Signal):
     def send(self, sender, **named):
         """Send the signal via Celery."""
 
+        logger = get_task_logger(__name__)
+        logger.info("START send")
+        logger.info(self)
+        logger.info(sender)
+        logger.info(_make_id(sender))
+        logger.info(self._live_receivers(_make_id(sender)))
+        
         for receiver in self._live_receivers(_make_id(sender)):
-        try:
-            logger.info("START Receiver: {}; Signal: {}; sender: {}, kwargs:{}".format(receiver,signal,sender,named))
-            call_receiver.apply_async(
-                args=(receiver, self, sender,),
-                kwargs=named,
-                queue=self.queue,
-            )
-            logger.info("END Receiver: {}; Signal: {}; sender: {}, kwargs:{}".format(receiver,signal,sender,named))
-        except Exception as ex:
-            logger.info("EXCEPT START Receiver: {}; Signal: {}; sender: {}, kwargs:{}".format(receiver,signal,sender,named))
-            logger.error(ex)
-            logger.info("EXCEPT END Receiver: {}; Signal: {}; sender: {}, kwargs:{}".format(receiver,signal,sender,named))
+            try:
+                logger.info("START Receiver: {}; Signal: {}; sender: {}, kwargs:{}".format(receiver,self,sender,named))
+                call_receiver.apply_async(
+                    args=(receiver, self, sender,),
+                    kwargs=named,
+                    queue=self.queue,
+                )
+                logger.info("END Receiver: {}; Signal: {}; sender: {}, kwargs:{}".format(receiver,self,sender,named))
+            except Exception as ex:
+                logger.info("EXCEPT START Receiver: {}; Signal: {}; sender: {}, kwargs:{}".format(receiver,self,sender,named))
+                logger.error(ex)
+                logger.info("EXCEPT END Receiver: {}; Signal: {}; sender: {}, kwargs:{}".format(receiver,self,sender,named))
     
+        logger.info("END send")
         
 #         propagate_signal.apply_async(
 #             args=(self, sender,),
