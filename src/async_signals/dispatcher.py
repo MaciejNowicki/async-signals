16ec4ac607704088057183ad3eabc5ce140f4c96
MaciejNowicki <maciejjnowicki@gmail.com>
2019-03-04 13:05:24 +0100

    Fix args

--- src/async_signals/dispatcher.py
+++ src/async_signals/dispatcher.py
@@ -1,8 +1,40 @@
-from celery import task
+from celery import shared_task
 from django.dispatch.dispatcher import (
     _make_id,
     Signal,
 )
+
+
+@shared_task
+def propagate_signal(self, sender, **named):
+    """
+    Send signal from sender to all connected receivers catching errors.
+
+    Arguments:
+
+            sender The sender of the signal. Can be any python object
+                (normally one registered with a connect if you
+                actually want something to occur).
+
+            named
+                Named arguments which will be passed to receivers. These
+                arguments must be a subset of the argument names defined in
+                providing_args.
+
+    Return a list of tuple pairs [(receiver, response), ... ]. May raise
+    DispatcherKeyError.
+
+    If any receiver raises an error (specifically any subclass of
+    Exception), the error instance is returned as the result for that
+    receiver.
+    """
+
+    # Call each receiver with whatever arguments it can accept.
+    for receiver in self._live_receivers(_make_id(sender)):
+        try:
+            receiver(signal=self, sender=sender, **named)
+        except Exception:
+            pass
 
 
 class AsyncSignal(Signal):
@@ -15,39 +47,8 @@ class AsyncSignal(Signal):
     def send(self, sender, **named):
         """Send the signal via Celery."""
 
-        self.propagate_signal.apply_async(
-            args=(sender,),
+        propagate_signal.apply_async(
+            args=(self, sender,),
             kwargs=named,
             queue=self.queue,
         )
-
-    @task
-    def propagate_signal(self, sender, **named):
-        """
-        Send signal from sender to all connected receivers catching errors.
-
-        Arguments:
-
-            sender The sender of the signal. Can be any python object
-                (normally one registered with a connect if you
-                actually want something to occur).
-
-            named
-                Named arguments which will be passed to receivers. These
-                arguments must be a subset of the argument names defined in
-                providing_args.
-
-        Return a list of tuple pairs [(receiver, response), ... ]. May raise
-        DispatcherKeyError.
-
-        If any receiver raises an error (specifically any subclass of
-        Exception), the error instance is returned as the result for that
-        receiver.
-        """
-
-        # Call each receiver with whatever arguments it can accept.
-        for receiver in self._live_receivers(_make_id(sender)):
-            try:
-                receiver(signal=self, sender=sender, **named)
-            except Exception:
-                pass
